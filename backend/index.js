1a2559e23a33484788c2b435102c66ce931e0b87
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-05 12:32:43 -0400

    .env

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const app = express();
 
+mongoose.connect(proccess.env.MONGO_URL, {useNewUrlParser: true});
+
 app.listen(8800,()=>{
     console.log("Backend server is running!")
 })
