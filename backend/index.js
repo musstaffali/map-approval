553018e12eb24607591c84022aec8bcbe9979aac
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-05 19:44:51 -0400

    mongo live

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,20 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// const userRoute = require("./routes/users");
-// const pinRoute = require("./routes/pins");
+const app = express();
 
 dotenv.config();
 
-app.use(express.json());
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() =>{
+    console.log("MongoDB Connected!");
+})
+.catch((err) => console.log(err));
 
-mongoose 
- .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,   })   
- .then(() => console.log("MongoDB connected!"))
- .catch(err => console.log(err));
-
-// app.use("/api/users", userRoute);
-// app.use("/api/pins", pinRoute);
-
-app.listen(8800, () => {
-  console.log("Backend server is running!");
-});
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// const app = express();
-
-// dotenv.config();
-
-// mongoose
-// .connect(proccess.env.MONGO_URL, { useNewUrlParser: true })
-// .then(() => {
-//     console.log("MongoDB Connected!")
-// })
-// .catch((err) => console.log(err));
-
-// app.listen(8800,()=>{
-//     console.log("Backend server is running!");
-// });
+    app.listen(9900, () => {
+        console.log("Backend server is running!");
+    });
