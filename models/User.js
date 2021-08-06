c32b548adcb756c6246349099bd0baaa7909a73d
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-06 15:28:55 -0400

    log in character requirement

--- models/User.js
+++ models/User.js
@@ -3,6 +3,22 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     username: {
         type:String,
-        require:true
-    }
-})
+        require:true,
+        min: 3,
+        max: 20,
+        unique: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type:String,
+        require,
+        min: 6,
+    },
+},
+{ timestamps: true }
+);
