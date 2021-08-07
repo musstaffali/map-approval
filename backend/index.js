fdda89331e49c83e56748bad4c628139efa76c2e
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-06 20:38:16 -0400

    react fix

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
 
 dotenv.config();
 
@@ -14,6 +16,9 @@ mongoose
     console.log("MongoDB Connected!");
 })
 .catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
     app.listen(8800, () => {
         console.log("Backend server is running!");
