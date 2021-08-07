4c387a93e4895d3b4a96482bbbace37e971e08f9
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-06 21:13:52 -0400

    reactApp

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ mongoose
 .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
 })
 .then(() =>{
     console.log("MongoDB Connected!");
