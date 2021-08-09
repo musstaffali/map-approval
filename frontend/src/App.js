7e1f23b646c8e06173ba3fa9dd95ac26fb16a10b
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-08-08 21:29:06 -0400

    cardbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { useState  } from "react";
-import ReactMapGL, { Marker } from "react-map-gl";
-import { Room } from "@material-ui/icons";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import "./app.css"
 
 function App() {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: 46,
+    longitude: 17,
     zoom: 4
   });
   return (
@@ -24,9 +25,32 @@ function App() {
         offsetLeft={-20}
         offsetTop={-10}
         >
-          <Room style={{fontSize:viewport.zoom *10}}/>
-          <div>You are here</div>
+          <Room style={{ fontSize: viewport.zoom * 7, color:"slateblue" }}/>
         </Marker>
+        <Popup
+          latitude={48.858093}
+          longitude={2.294694}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="bottom" >
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">Eiffell Tower. I like it.</h4>
+            <label>Review</label>
+            <p>Beautiful Place</p>
+            <label>Rating</label>
+            <div className="stars">
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>safak</b></span>
+            <span className="date">1 hour ago</span>
+          </div>
+        </Popup>
         </ReactMapGL>
     </div>
   );
